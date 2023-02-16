90fb3b58261d87cb6243ecbbdd07216772e37dbb
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2023-02-17 00:23:50 +0400

    add client app ci support

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.post('/api/webhook', (req, res)=> {
         if (stderr) {
             console.log(stderr);
             mailTransporter({ 
-            subject: 'The build process failed',
+            subject: 'The backend app build process failed',
             html: `<h2>The build process failed</h2><p>${stderr}</p>`
         })
             return res.status(500).send('error in git pull command');
@@ -27,14 +27,14 @@ app.post('/api/webhook', (req, res)=> {
             if (stderr) {
                 console.error(stderr);
                 mailTransporter({ 
-                    subject: 'The build process failed',
+                    subject: 'The backedn app build process failed',
                     html: `<h2>The build process failed</h2><p>${stderr}</p>`
                 })
                 return res.status(500).send('err in pm2 command');
             }
              console.log(stdout);
              mailTransporter({ 
-                subject: 'The build process are successfull!',
+                subject: 'The backend app build process is successfull!',
                 html: `<h2>Greetings from Abdurrahim's CI|CD app, look at your site! :)</h2>`
             })
         
@@ -72,7 +72,7 @@ app.post('/admin/webhook', (req, res) => {
             console.log(stderr);
             mailTransporter({ 
                 subject: 'The build process failed',
-                html: `<h2>The build process failed</h2><p>${stderr}</p>`
+                html: `<h2>The admin app build process failed</h2><p>${stderr}</p>`
             })
             return res.status(500).send('error in build command');
         }
@@ -86,7 +86,7 @@ app.post('/admin/webhook', (req, res) => {
         if (stderr) {
             console.log(stderr);
             mailTransporter({ 
-                subject: 'The build process failed',
+                subject: 'The admin app build process failed',
                 html: `<h2>The build process failed</h2><p>${stderr}</p>`
             })
             return res.status(500).send('error in cd /var/wwww command');
@@ -102,14 +102,14 @@ app.post('/admin/webhook', (req, res) => {
         if (stderr) {
             console.log(stderr);
             mailTransporter({ 
-                subject: 'The build process failed',
+                subject: 'The admin app build process failed',
                 html: `<h2>The build process failed</h2><p>${stderr}</p>`
             })
             return res.status(500).send('error in replacing admin folder with the build');
         }
         console.log(stdout);
         mailTransporter({ 
-            subject: 'The build process are successfull!',
+            subject: 'The admin app build process are successfull!',
             html: `<h2>Greetings from Abdurrahim's CI|CD app, look at your site! :)</h2>`
         })
         res.status(200).send('successfull');
@@ -119,44 +119,111 @@ app.post('/admin/webhook', (req, res) => {
     })
 })
 
-app.post('/backend/ci', (req, res) => {
-    exec('cd ~/masterDevops && git pull', (err, stdout, stderr) => {
-        if (err) {
+app.post('/client/webhook', (req, res) => {
+    exec('cd ~/URSwebsite && git pull', (err, stdout, stderr) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).send('errror');
+        }
+        if (stderr) {
+            console.log(stderr);
+            mailTransporter({ 
+                subject: 'The client app build process failed',
+                html: `<h2>The build process failed</h2><p>${stderr}</p>`
+            })
+            return res.status(500).send('error in git pull command');
+        }
+        console.log(stdout);
+
+        exec('cd ~/URSwebsite && npm run build', (err, stdout, stderr) => {
+              if (err) {
             console.error(err);
             return res.status(500).send('error');
         }
         if (stderr) {
             console.log(stderr);
             mailTransporter({ 
-            subject: 'The build process failed',
-            html: `<h2>The build process failed</h2><p>${stderr}</p>`
-        })
-            return res.status(500).send('error in git pull command');
+                subject: 'The client app build process failed',
+                html: `<h2>The client app build process failed</h2><p>${stderr}</p>`
+            })
+            return res.status(500).send('error in build command');
         }
         console.log(stdout);
 
-        exec('pm2 restart server', (err, stdout, stderr) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).send('error');
-            }
-            if (stderr) {
-                console.error(stderr);
-                mailTransporter({ 
-                    subject: 'The build process failed',
-                    html: `<h2>The build process failed</h2><p>${stderr}</p>`
-                })
-                return res.status(500).send('err in pm2 command');
-            }
-             console.log(stdout);
-             mailTransporter({ 
-                subject: 'The build process are successfull!',
-                html: `<h2>Greetings from Abdurrahim's CI|CD app, look at your site! :)</h2>`
+        exec('cd ~/URSwebsite && sudo mv build /var/www && cd /var/www', (err, stdout, stderr) => {
+              if (err) {
+            console.error(err);
+            return res.status(500).send('error');
+        }
+        if (stderr) {
+            console.log(stderr);
+            mailTransporter({ 
+                subject: 'The client app build process failed',
+                html: `<h2>The build process failed</h2><p>${stderr}</p>`
             })
-        
-             return res.status(200).send('successfull');
+            return res.status(500).send('error in cd /var/wwww command');
+        }
+        console.log(stdout);
+        // send email to user
+
+        exec('cd /var/www && sudo rm -rf client && sudo mv build client', (err, stdout, stderr) => {
+              if (err) {
+            console.error(err);
+            return res.status(500).send('error');
+        }
+        if (stderr) {
+            console.log(stderr);
+            mailTransporter({ 
+                subject: 'The client app build process failed',
+                html: `<h2>The build process failed</h2><p>${stderr}</p>`
+            })
+            return res.status(500).send('error in replacing admin folder with the build');
+        }
+        console.log(stdout);
+        mailTransporter({ 
+            subject: 'The client app build process are successfull!',
+            html: `<h2>Greetings from Abdurrahim's CI|CD app, look at your site! :)</h2>`
+        })
+        res.status(200).send('successfull');
+        })
+        })
         })
     })
 })
+
+// app.post('/backend/ci', (req, res) => {
+//     exec('cd ~/masterDevops && git pull', (err, stdout, stderr) => {
+//         if (err) {
+//             console.error(err);
+//             return res.status(500).send('error');
+//         }
+//         if (stderr) {
+//             console.log(stderr);
+//             mailTransporter({ 
+//             subject: 'The build process failed',
+//             html: `<h2>The build process failed</h2><p>${stderr}</p>`
+//         })
+//             return res.status(500).send('error in git pull command');
+//         }
+//         console.log(stdout);
+
+//         exec('pm2 restart server', (err, stdout, stderr) => {
+//             if (err) {
+//                 console.log(err);
+//                 return res.status(500).send('error');
+//             }
+//             if (stderr) {
+//                 console.error(stderr);
+//                 mailTransporter({ 
+//                     subject: 'The build process failed',
+//                     html: `<h2>The build process failed</h2><p>${stderr}</p>`
+//                 })
+//                 return res.status(500).send('err in pm2 command');
+//             }
+//              console.log(stdout);
+        
+//         })
+//     })
+// })
 
 module.exports = app;
