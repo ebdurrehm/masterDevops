3277f8ab51045ef036fa6a973f2fb741e6e53da5
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2023-02-17 02:10:16 +0400

    fix: sudo require passwd for client hook

--- src/app.js
+++ src/app.js
@@ -121,75 +121,75 @@ app.post('/admin/webhook', (req, res) => {
 })
 
 app.post('/client/webhook', (req, res) => {
-    exec('cd ~/URSwebsite && git pull', (err, stdout, stderr) => {
-          if (err) {
-            console.error(err);
-            return res.status(500).send('errror');
-        }
-        if (stderr) {
-            console.log(stderr);
-            mailTransporter({ 
-                subject: 'The client app build process failed',
-                html: `<h2>The build process failed</h2><p>${stderr}</p>`
-            })
-            return res.status(500).send('error in git pull command');
-        }
-        console.log(stdout);
+    exec(`cd ~/URSwebsite && git pull`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('errror');
+      }
+      if (stderr) {
+          console.log(stderr);
+          // mailTransporter({ 
+          //     subject: 'The build process failed',
+          //     html: `<h2>The build process failed</h2><p>${stderr}</p>`
+          // })
+          // return res.status(500).send('error in git pull command');
+      }
+      console.log(stdout);
 
-        exec('cd ~/URSwebsite && npm run build', (err, stdout, stderr) => {
-              if (err) {
-            console.error(err);
-            return res.status(500).send('error');
-        }
-        if (stderr) {
-            console.log(stderr);
-            mailTransporter({ 
-                subject: 'The client app build process failed',
-                html: `<h2>The client app build process failed</h2><p>${stderr}</p>`
-            })
-            return res.status(500).send('error in build command');
-        }
-        console.log(stdout);
+      exec('cd ~/URSwebsite && npm run build', (err, stdout, stderr) => {
+            if (err) {
+          console.error(err);
+          return res.status(500).send('error');
+      }
+      if (stderr) {
+          console.log(stderr);
+          // mailTransporter({ 
+          //     subject: 'The build process failed',
+          //     html: `<h2>The admin app build process failed</h2><p>${stderr}</p>`
+          // })
+          // return res.status(500).send('error in build command');
+      }
+      console.log(stdout);
 
-        exec(`cd ~/URSwebsite && echo ${ck.SERVER_PASSWRD} | sudo -S mv build /var/www && cd /var/www`, (err, stdout, stderr) => {
-              if (err) {
-            console.error(err);
-            return res.status(500).send('error');
-        }
-        if (stderr) {
-            console.log(stderr);
-            mailTransporter({ 
-                subject: 'The client app build process failed',
-                html: `<h2>The build process failed</h2><p>${stderr}</p>`
-            })
-            return res.status(500).send('error in cd /var/wwww command');
-        }
-        console.log(stdout);
-        // send email to user
+      exec(`cd ~/URSwebsite && echo ${ck.SERVER_PASSWRD} | sudo -S mv build /var/www && cd /var/www`, (err, stdout, stderr) => {
+            if (err) {
+          console.error(err);
+          return res.status(500).send('error');
+      }
+      if (stderr) {
+          console.log(stderr);
+          // mailTransporter({ 
+          //     subject: 'The admin app build process failed',
+          //     html: `<h2>The build process failed</h2><p>${stderr}</p>`
+          // })
+          // return res.status(500).send('error in cd /var/wwww command');
+      }
+      console.log(stdout);
+      // send email to user
 
-        exec(`cd /var/www && echo ${ck.SERVER_PASSWRD} | sudo -S rm -rf client && sudo mv build client`, (err, stdout, stderr) => {
-              if (err) {
-            console.error(err);
-            return res.status(500).send('error');
-        }
-        if (stderr) {
-            console.log(stderr);
-            mailTransporter({ 
-                subject: 'The client app build process failed',
-                html: `<h2>The build process failed</h2><p>${stderr}</p>`
-            })
-            return res.status(500).send('error in replacing admin folder with the build');
-        }
-        console.log(stdout);
-        mailTransporter({ 
-            subject: 'The client app build process are successfull!',
-            html: `<h2>Greetings from Abdurrahim's CI|CD app, look at your site! :)</h2>`
-        })
-        res.status(200).send('successfull');
-        })
-        })
-        })
-    })
+      exec(`cd /var/www && echo ${ck.SERVER_PASSWRD} | sudo -S rm -rf client && sudo mv build client`, (err, stdout, stderr) => {
+            if (err) {
+          console.error(err);
+          return res.status(500).send('error');
+      }
+      if (stderr) {
+          console.log(stderr);
+          // mailTransporter({ 
+          //     subject: 'The admin app build process failed',
+          //     html: `<h2>The build process failed</h2><p>${stderr}</p>`
+          // })
+          // return res.status(500).send('error in replacing admin folder with the build');
+      }
+      console.log(stdout);
+      mailTransporter({ 
+          subject: 'The admin app build process are successfull!',
+          html: `<h2>Greetings from Abdurrahim's CI|CD app, look at your site! :)</h2>`
+      })
+      res.status(200).send('successfull');
+      })
+      })
+      })
+  })
 })
 
 // app.post('/backend/ci', (req, res) => {
